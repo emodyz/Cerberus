bd33704e554ecd8c33b3676f3b805da76ae3287b
Saber Nouira <contact@wirk.fr>
2021-01-30 19:15:35 +0100

    Add authorizations attribute to coresponding trait

--- src/Traits/HasAuthorizations.php
+++ src/Traits/HasAuthorizations.php
@@ -3,17 +3,24 @@
 namespace Emodyz\Cerberus\Traits;
 
 use Emodyz\Cerberus\Facades\Cerberus;
+use Illuminate\Support\Collection;
 
 trait HasAuthorizations
 {
     public function checkAuthorizationTo(string $action): bool
     {
-        // $subjectId = $this->{$this->getKeyName()};
-
         $role = $this->getAttribute('role');
 
         $can = Cerberus::config('roles.'.$role.'.can');
 
         return $can->contains($action) || $can->contains('*');
+    }
+
+
+    public function getAuthorizationsAttribute(): Collection
+    {
+        $role = $this->getAttribute('role');
+
+        return Cerberus::config('roles.'.$role.'.can');
     }
 }
